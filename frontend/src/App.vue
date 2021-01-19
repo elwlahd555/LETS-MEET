14cf40fc01130a12b301d13257d6e65a98659aac
Lee-sungheon <leesungheon.dev@gmail.com>
2021-01-19 18:04:38 +0900

    0119_FE : first commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,93 @@
+<template>
+  <v-app>
+    <div>
+      <v-app-bar
+        color="white"
+        dense
+        dark
+        fixed
+        style="z-index:10;"
+      >
+        <v-toolbar-title><img src="./assets/images/logo.png" 
+        style="width: 35%; max-height: 50px; max-width: 200px;">
+        </v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon style="color: black;">mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-menu
+          left
+          bottom
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              icon
+              style="color: black;"
+              v-bind="attrs"
+              v-on="on"
+            >
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </template>
+
+          <v-list>
+            <v-list-item
+              v-for="n in 5"
+              :key="n"
+              @click="() => {}"
+            >
+              <v-list-item-title>Option {{ n }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-app-bar>
+    </div>
+    
+    <v-main>
+      <Main class="mt-13 mb-13"/>
+    </v-main>
+
+    <v-bottom-navigation
+      :value="value"
+      color="teal"
+      dense
+      grow
+      fixed
+      style="z-index:10;"
+    >
+      <v-btn>
+        <span></span>
+        <v-icon>mdi-home</v-icon>
+      </v-btn>
+      <v-btn>
+        <v-icon>mdi-calendar</v-icon>
+      </v-btn>
+      <v-btn>
+        <v-icon>mdi-plus-circle</v-icon>
+      </v-btn>
+      <v-btn>
+        <v-icon>mdi-bell</v-icon>
+      </v-btn>
+      <v-btn>
+        <v-icon>mdi-account-circle</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </v-app>
+</template>
+
+<script>
+import Main from './views/main/Main.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    Main,
+  },
+
+  data: () => ({ value: 0 }),
+};
+</script>
---
+++ frontend/src/views/main/Main.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <v-card
+      class="mx-auto mt-3"
+      v-for="idx in list"
+      :key="idx"
+    >
+      <v-img 
+      height="200px"
+      src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+      style="filter: brightness(50%);">
+      </v-img>
+     <v-fade-transition>
+        <v-overlay
+          absolute >
+          <div class="d-flex flex-column mt-5">
+            <div class="d-flex justify-center"><v-icon class="mb-1">mdi-food</v-icon> 밥 약속</div>
+            <div class="d-flex justify-center"><h4>이모저모 런닝 모임</h4></div>
+            <div class="d-flex justify-center">2021.01.14</div>
+            <div class="d-flex justify-center">5명</div>
+          </div>
+        </v-overlay>
+      </v-fade-transition>
+    </v-card>
+    <infinite-loading @infinite="infiniteHandler"></infinite-loading>
+  </div>
+</template>
+
+<script>
+import InfiniteLoading from 'vue-infinite-loading'
+
+export default {
+  name: "Main",
+  data() {
+    return {
+      list: [1, 2, 3]
+    };
+  },
+  components: {
+    InfiniteLoading,
+  },
+   methods: {
+    infiniteHandler($state) {
+      setTimeout(() => {
+        const temp = [];
+        for (let i = this.list.length + 1; i <= this.list.length + 3; i++) {
+          temp.push(i);
+        }
+        this.list = this.list.concat(temp);
+        $state.loaded();
+      }, 1000);
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
