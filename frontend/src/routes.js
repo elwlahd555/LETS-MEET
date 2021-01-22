b57b16f112c7bf1fed7fe1d10e8a575b3fd6b046
이주이 <dea8307@naver.com>
2021-01-22 13:55:21 +0900

    0122_FE : 로그인

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,9 +1,16 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
 import Main from './views/main/Main.vue'
-
+import Login from './views/user/Login.vue'
 export default [
 
     {
         path : '/',
+        name : 'Login',
+        component : Login
+    },  
+    {
+        path : '/main',
         name : 'Main',
         component : Main
     },
