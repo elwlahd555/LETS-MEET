db0b48f350255e2af67a6f3b4a4bad17b7da7dae
Lee-sungheon <leesungheon.dev@gmail.com>
2021-01-25 17:49:38 +0900

    0125_FE : 방만들기

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,17 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
-import { store } from "./store";
+import { store } from "./store"
+import router from './routes'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueGeolocationApi from 'vue-geolocation-api'
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(VueGeolocationApi/*, { ...options } */)
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  router,
   store: store,
   render: h => h(App)
 }).$mount('#app')
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,8 +1,12 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Main from './views/main/Main.vue'
-import Login from './views/user/Login.vue'
-export default [
+import Vue from "vue"
+import VueRouter from "vue-router"
+import Main from '@/views/main/Main'
+import Login from '@/views/user/Login'
+import MakeMeeting from '@/views/makemeeting/MakeMeeting'
+
+Vue.use(VueRouter)
+
+const routes = [
     {
         path : '/',
         name : 'Login',
@@ -13,4 +17,17 @@ export default [
         name : 'Main',
         component : Main
     },
-]
+    {
+        path : '/makemeeting',
+        name : 'MakeMeeting',
+        component : MakeMeeting
+    },
+  ]
+  
+  const router = new VueRouter({
+    base: process.env.BASE_URL,
+    // mode: 'history',
+    routes
+  })
+
+export default router
