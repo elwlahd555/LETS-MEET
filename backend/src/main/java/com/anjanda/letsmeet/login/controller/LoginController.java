1e714478852b97300096c428de015fa2a3557dac
goldeneastlight <goldeneastlight@gmail.com>
2021-01-27 17:13:54 +0900

    0127_BE : login backend 오류 해결

--- backend/src/main/java/com/anjanda/letsmeet/login/controller/LoginController.java
+++ backend/src/main/java/com/anjanda/letsmeet/login/controller/LoginController.java
@@ -7,17 +7,17 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.anjanda.letsmeet.login.service.LoginService;
 import com.anjanda.letsmeet.repository.dto.User;
 
-@Controller
+@RestController
 @RequestMapping("/login")
 public class LoginController {
 	@Autowired
