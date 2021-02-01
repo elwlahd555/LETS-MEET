b0dd37e9a7aa1af7c52fd7db1d4a3ec65a9a6a54
goldeneastlight <goldeneastlight@gmail.com>
2021-02-01 14:33:37 +0900

    0201_BE : Google, Kakao Login 구현중

--- backend/src/main/java/com/anjanda/letsmeet/login/controller/LoginController.java
+++ backend/src/main/java/com/anjanda/letsmeet/login/controller/LoginController.java
@@ -1,6 +1,5 @@
 package com.anjanda.letsmeet.login.controller;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.Cookie;
@@ -8,9 +7,13 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,17 +21,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import com.anjanda.letsmeet.login.service.LoginService;
-import com.anjanda.letsmeet.repository.dto.GoogleOAuthRequest;
-import com.anjanda.letsmeet.repository.dto.GoogleOAuthResponse;
 import com.anjanda.letsmeet.repository.dto.User;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 
 @RestController
 @RequestMapping("/login")
@@ -67,91 +62,128 @@ public class LoginController {
 		}
 		return "redirect:/main"; // 메인으로 리다이렉트 보낸다.
 	}
-
-	// googleLogin
-	final static String GOOGLE_AUTH_BASE_URL = "https://accounts.google.com/o/oauth2/v2/auth";
-	final static String GOOGLE_TOKEN_BASE_URL = "https://oauth2.googleapis.com/token";
-	final static String GOOGLE_REVOKE_TOKEN_BASE_URL = "https://oauth2.googleapis.com/revoke";
-
-	@Value("${api.client_id}")
-	String clientId;
-	@Value("${api.client_secret}")
-	String clientSecret;
-
-
-	/**
-	 * Authentication Code를 전달 받는 엔드포인트
-	 **/
-	@GetMapping("google/auth")
-	public String googleAuth(Model model, @RequestParam(value = "code") String authCode)
-			throws JsonProcessingException {
+//
+//	// googleLogin
+//	final static String GOOGLE_AUTH_BASE_URL = "https://accounts.google.com/o/oauth2/v2/auth";
+//	final static String GOOGLE_TOKEN_BASE_URL = "https://oauth2.googleapis.com/token";
+//	final static String GOOGLE_REVOKE_TOKEN_BASE_URL = "https://oauth2.googleapis.com/revoke";
+//
+//	@Value("${api.client_id}")
+//	String clientId;
+//	@Value("${api.client_secret}")
+//	String clientSecret;
+//
+//
+//	/**
+//	 * Authentication Code를 전달 받는 엔드포인트
+//	 **/
+//	@GetMapping("google/auth")
+//	public String googleAuth(Model model, @RequestParam(value = "code") String authCode)
+//			throws JsonProcessingException {
+//		
+//		//HTTP Request를 위한 RestTemplate
+//		RestTemplate restTemplate = new RestTemplate();
+//
+//		//Google OAuth Access Token 요청을 위한 파라미터 세팅
+//		GoogleOAuthRequest googleOAuthRequestParam = GoogleOAuthRequest
+//				.builder()
+//				.clientId(clientId)
+//				.clientSecret(clientSecret)
+//				.code(authCode)
+//				.redirectUri("http://localhost:8080/login/google/auth")
+//				.grantType("authorization_code").build();
+//
+//		
+//		//JSON 파싱을 위한 기본값 세팅
+//		//요청시 파라미터는 스네이크 케이스로 세팅되므로 Object mapper에 미리 설정해준다.
+//		ObjectMapper mapper = new ObjectMapper();
+//		mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+//		mapper.setSerializationInclusion(Include.NON_NULL);
+//
+//		//AccessToken 발급 요청
+//		ResponseEntity<String> resultEntity = restTemplate.postForEntity(GOOGLE_TOKEN_BASE_URL, googleOAuthRequestParam, String.class);
+//
+//		//Token Request
+//		GoogleOAuthResponse result = mapper.readValue(resultEntity.getBody(), new TypeReference<GoogleOAuthResponse>() {
+//		});
+//		
+//		System.out.println(resultEntity.getBody());
+//
+//		//ID Token만 추출 (사용자의 정보는 jwt로 인코딩 되어있다)
+//		String jwtToken = result.getIdToken();
+//		String requestUrl = UriComponentsBuilder.fromHttpUrl("https://oauth2.googleapis.com/tokeninfo")
+//		.queryParam("id_token", jwtToken).encode().toUriString();
+//		
+//		String resultJson = restTemplate.getForObject(requestUrl, String.class);
+//		
+//		Map<String,String> userInfo = mapper.readValue(resultJson, new TypeReference<Map<String, String>>(){});
+//		model.addAllAttributes(userInfo);
+//		model.addAttribute("token", result.getAccessToken());
+//		System.out.println(userInfo);
+//
+//
+//
+//
+//		return "/google.html";
+//
+//	}
+//
+//	/**
+//	 * 토큰 무효화 
+//	 **/
+//	@GetMapping("google/revoke/token")
+//	@ResponseBody
+//	public Map<String, String> revokeToken(@RequestParam(value = "token") String token) throws JsonProcessingException {
+//
+//		Map<String, String> result = new HashMap<>();
+//		RestTemplate restTemplate = new RestTemplate();
+//		final String requestUrl = UriComponentsBuilder.fromHttpUrl(GOOGLE_REVOKE_TOKEN_BASE_URL)
+//				.queryParam("token", token).encode().toUriString();
+//		
+//		System.out.println("TOKEN ? " + token);
+//		
+//		String resultJson = restTemplate.postForObject(requestUrl, null, String.class);
+//		result.put("result", "success");
+//		result.put("resultJson", resultJson);
+//
+//		return result;
+//
+//	}
+//	
+	@GetMapping("/auth/kakao/callback")
+	public @ResponseBody String kakaoCallback(String code) { // Data를 return 해주는 controller method
+//		System.out.println("카카오 인증 완료 : 코드값 : " + code);
 		
-		//HTTP Request를 위한 RestTemplate
-		RestTemplate restTemplate = new RestTemplate();
-
-		//Google OAuth Access Token 요청을 위한 파라미터 세팅
-		GoogleOAuthRequest googleOAuthRequestParam = GoogleOAuthRequest
-				.builder()
-				.clientId(clientId)
-				.clientSecret(clientSecret)
-				.code(authCode)
-				.redirectUri("http://localhost:8080/login/google/auth")
-				.grantType("authorization_code").build();
-
+		// POST방식으로 key=value 데이터를 요청 (카카오쪽으로)
+		// Restrofit2
+		// OkHttp
+		// RestTemplate
 		
-		//JSON 파싱을 위한 기본값 세팅
-		//요청시 파라미터는 스네이크 케이스로 세팅되므로 Object mapper에 미리 설정해준다.
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
-		mapper.setSerializationInclusion(Include.NON_NULL);
-
-		//AccessToken 발급 요청
-		ResponseEntity<String> resultEntity = restTemplate.postForEntity(GOOGLE_TOKEN_BASE_URL, googleOAuthRequestParam, String.class);
-
-		//Token Request
-		GoogleOAuthResponse result = mapper.readValue(resultEntity.getBody(), new TypeReference<GoogleOAuthResponse>() {
-		});
+		RestTemplate rt = new RestTemplate();
 		
-		System.out.println(resultEntity.getBody());
-
-		//ID Token만 추출 (사용자의 정보는 jwt로 인코딩 되어있다)
-		String jwtToken = result.getIdToken();
-		String requestUrl = UriComponentsBuilder.fromHttpUrl("https://oauth2.googleapis.com/tokeninfo")
-		.queryParam("id_token", jwtToken).encode().toUriString();
+		// HttpHeader 오브젝트 생성
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
 		
-		String resultJson = restTemplate.getForObject(requestUrl, String.class);
+		// HttpBody 오브젝트 생성
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add("grant_type", "authorization_code");
+		params.add("client_id", "deace353f1fd1555201a07f914bc5598");
+		params.add("redirect_uri", "http://localhost:8080/auth/kakao/callback");
+		params.add("code", "code");
 		
-		Map<String,String> userInfo = mapper.readValue(resultJson, new TypeReference<Map<String, String>>(){});
-		model.addAllAttributes(userInfo);
-		model.addAttribute("token", result.getAccessToken());
-		System.out.println(userInfo);
-
-
-
-
-		return "/google.html";
-
-	}
-
-	/**
-	 * 토큰 무효화 
-	 **/
-	@GetMapping("google/revoke/token")
-	@ResponseBody
-	public Map<String, String> revokeToken(@RequestParam(value = "token") String token) throws JsonProcessingException {
-
-		Map<String, String> result = new HashMap<>();
-		RestTemplate restTemplate = new RestTemplate();
-		final String requestUrl = UriComponentsBuilder.fromHttpUrl(GOOGLE_REVOKE_TOKEN_BASE_URL)
-				.queryParam("token", token).encode().toUriString();
+		// HttpHeader와 HttpBody를 하나의 오브젝트에 담기
+		HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest =
+				new HttpEntity<>(params, headers);
 		
-		System.out.println("TOKEN ? " + token);
+		// Http 요청하기 - Post방식으로 - 그리고 response 변수의 응답 받음.
+		ResponseEntity<String> response = rt.exchange(
+				"https://kauth.kakao.com/oauth/token",
+				HttpMethod.POST,
+				kakaoTokenRequest,
+				String.class
+		);
 		
-		String resultJson = restTemplate.postForObject(requestUrl, null, String.class);
-		result.put("result", "success");
-		result.put("resultJson", resultJson);
-
-		return result;
-
+		return "카카오 토큰 요청 완료 : 토큰요청에 대한 응답 : " + response;
 	}
 }
--- backend/src/main/java/com/anjanda/letsmeet/repository/dto/User.java
+++ backend/src/main/java/com/anjanda/letsmeet/repository/dto/User.java
@@ -1,39 +1,53 @@
 package com.anjanda.letsmeet.repository.dto;
 
-import lombok.Builder;
-import lombok.Data;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import org.springframework.data.annotation.Id;
 
 /**
  * 
  * @Date 2021. 1. 25.
  * @Team AnJanDa
  * @author 개발자명
- * @Project : Test - 
+ * @Project : Test -
  * @Function : 유저 DB 테이블
- * @Description : 
- *	- 이메일 인증 확인 => 수정 필요
- *	- 사진 받기 호우...?
+ * @Description : - 이메일 인증 확인 => 수정 필요 - 사진 받기 호우...?
  */
 
-@Data
-@Builder
+@Entity
 public class User {
-	private int uNo;
+
+	@Id // Primary key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // 프로젝트에서 연결된 DB의 넘버링 전략을 따라간다.
+	private int uNo; // 시퀀스, auto_increment
+
+	@Column
 	private String uEmail;
+	@Column
 	private char uConfirmEmail;
+	@Column
 	private String uPassword;
+	@Column
 	private String uName;
+	@Column
 	private String uBirth;
+	@Column
 	private String uJoinDate;
+	@Column
 	private String uImg;
+	@Column
 	private String uAddress;
+	@Column
 	private String uProvider;
-	
+
 	public User() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	public User(int uNo, String uEmail, char uConfirmEmail, String uPassword, String uName, String uBirth,
 			String uJoinDate, String uImg, String uAddress, String uProvider) {
 		super();
@@ -48,67 +62,87 @@ public class User {
 		this.uAddress = uAddress;
 		this.uProvider = uProvider;
 	}
+
 	public int getuNo() {
 		return uNo;
 	}
+
 	public void setuNo(int uNo) {
 		this.uNo = uNo;
 	}
+
 	public String getuEmail() {
 		return uEmail;
 	}
+
 	public void setuEmail(String uEmail) {
 		this.uEmail = uEmail;
 	}
+
 	public char getuConfirmEmail() {
 		return uConfirmEmail;
 	}
+
 	public void setuConfirmEmail(char uConfirmEmail) {
 		this.uConfirmEmail = uConfirmEmail;
 	}
+
 	public String getuPassword() {
 		return uPassword;
 	}
+
 	public void setuPassword(String uPassword) {
 		this.uPassword = uPassword;
 	}
+
 	public String getuName() {
 		return uName;
 	}
+
 	public void setuName(String uName) {
 		this.uName = uName;
 	}
+
 	public String getuBirth() {
 		return uBirth;
 	}
+
 	public void setuBirth(String uBirth) {
 		this.uBirth = uBirth;
 	}
+
 	public String getuJoinDate() {
 		return uJoinDate;
 	}
+
 	public void setuJoinDate(String uJoinDate) {
 		this.uJoinDate = uJoinDate;
 	}
+
 	public String getuImg() {
 		return uImg;
 	}
+
 	public void setuImg(String uImg) {
 		this.uImg = uImg;
 	}
+
 	public String getuAddress() {
 		return uAddress;
 	}
+
 	public void setuAddress(String uAddress) {
 		this.uAddress = uAddress;
 	}
+
 	public String getuProvider() {
 		return uProvider;
 	}
+
 	public void setuProvider(String uProvider) {
 		this.uProvider = uProvider;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "UserDTO [uNo=" + uNo + ", uEmail=" + uEmail + ", uConfirmEmail=" + uConfirmEmail + ", uPassword="
