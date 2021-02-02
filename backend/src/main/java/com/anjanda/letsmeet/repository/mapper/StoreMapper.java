0933bfe14cda50e89f29f84b0516b730955ab2e2
DESKTOP-KVCQHCD\multicampus <ghqls0702@naver.com>
2021-02-02 20:04:25 +0900

    2020_BE : 백앤드 소켓 해결

--- backend/src/main/java/com/anjanda/letsmeet/repository/mapper/StoreMapper.java
+++ backend/src/main/java/com/anjanda/letsmeet/repository/mapper/StoreMapper.java
@@ -2,8 +2,6 @@ package com.anjanda.letsmeet.repository.mapper;
 
 import java.util.List;
 
-import org.springframework.stereotype.Repository;
-
 import com.anjanda.letsmeet.repository.dto.Store;
 /**
  * 
