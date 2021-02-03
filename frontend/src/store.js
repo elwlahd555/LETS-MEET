38addf61a30465a52d48cb6ee1263a651d0d0df9
이주이 <dea8307@naver.com>
2021-02-04 02:25:33 +0900

    0204 FE_check_it

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,25 +3,29 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
-const axios = require('axios');
 import createPersistedState from 'vuex-persistedstate'
 export const store = new Vuex.Store({
   plugins: [
+    // storage 에 넣어준당
     createPersistedState()
   ],
   state: {
-    email: null,
-    name: null,
+    uEmail: null,
+    uName: null,
     isLogin: false,
-    counter: 0
   },
+  // getters: {
+  //   get_user_name(state){
+  //     return state.uName
+  //   },
+  //   get_user_email(state){
+  //     return state.uEmail
+  // }
   mutations: {
     SET_USER_AUTH_DATA(state, payload){
       state.isLogin = true;
-      console.log(payload)
-      state.email = payload['uEmail']
-      state.name = payload['uName']
-      console.log(state)
+      state.uEmail = payload['uEmail']
+      state.uName = payload['uName']
     },
     SET_USER_AUTU_DATA_LOGOUT(state) {
       state.isLogin = false;
@@ -30,20 +34,24 @@ export const store = new Vuex.Store({
     }
   },
   actions: {
-    LOGIN (context, user) {
-      return axios
-      .post(`http://localhost:8000/letsmeet/login`, user)
-      .then((res) => {
-        alert('로그인 되었습니다.')
-        context.commit('SET_USER_AUTH_DATA', res.data)
-        let token = res.data['auth-token']
-        localStorage.setItem('auth-token', token)
-        // axios default 헤더에 현재 token 적재
-        axios.defaults.headers.common[
-            'auth-token'
-          ] = window.localStorage.getItem("auth-token");  
-      })
-    },
+    
+    // LOGIN (context, user) {
+    //   return axios
+    //   .post(`http://localhost:8000/letsmeet/login`, user)
+    //   .then((res) => {
+    //     let token = res.data['auth-token']
+    //     if (token === undefined) {
+    //       alert('비밀번호가 틀렸습니다.')
+    //     }
+    //     else {
+    //       alert('로그인 되었습니다.')
+    //       context.commit('SET_USER_AUTH_DATA', res.data)
+    //       localStorage.setItem('auth-token', token)
+    //       // axios default 헤더에 현재 token 적재
+    //       axios.defaults.headers.common['auth-token'] = window.localStorage.getItem("auth-token");
+    //       return true
+    //     }
+    //   })
     LOGOUT(context) {
       context.commit('SET_USER_AUTU_DATA_LOGOUT')
       localStorage.removeItem('auth-token')
