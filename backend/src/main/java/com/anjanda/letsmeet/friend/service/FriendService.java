fb3d0fb7115eaadb0f04ff83352255b8d1f72d2b
김지현 <khyun7621@naver.com>
2021-02-04 21:47:44 +0900

    friend 관련 MVC 작업

--- backend/src/main/java/com/anjanda/letsmeet/friend/service/FriendService.java
+++ backend/src/main/java/com/anjanda/letsmeet/friend/service/FriendService.java
@@ -2,18 +2,17 @@ package com.anjanda.letsmeet.friend.service;
 
 import java.util.List;
 
-import com.anjanda.letsmeet.repository.dto.Friend;
 import com.anjanda.letsmeet.repository.dto.User;
 
 public interface FriendService {
 
 	/* C :: 사용자의 친구 추가 */
-	int createFriend(Friend friend) throws Exception;
+	int createFriend(int me, User user) throws Exception;
 
 	/* R :: 사용자의 친구 조회 */
-	List<User> reviewFriend(Friend friend) throws Exception;
+	List<User> reviewFriend(User user) throws Exception;
 
 	/* D :: 사용자의 친구 삭제 */
-	boolean deleteFriend(Friend friend) throws Exception;
+	boolean deleteFriend(int me, User user) throws Exception;
 
 }
