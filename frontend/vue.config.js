b4519e8130236af50f609d5c87384b3f9b42749b
goldeneastlight <goldeneastlight@gmail.com>
2021-02-04 18:39:15 +0900

    fix

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,3 +3,14 @@ module.exports = {
     "vuetify"
   ]
 }
+module.exports = {
+  configureWebpack: {
+  // other webpack options to merge in ...
+  },
+  // devServer options dont belong into `configureWebpack`
+  devServer: {
+  host: "0.0.0.0",
+  hot: true,
+  disableHostCheck: true
+  },
+};
