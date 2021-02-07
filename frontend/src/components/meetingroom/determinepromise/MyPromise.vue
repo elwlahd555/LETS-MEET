368ccb1ce53f90912625d92e01e039abff13ff9f
Lee-sungheon <leesungheon.dev@gmail.com>
2021-02-07 23:50:48 +0900

    0207_FE : 미팅룸_약속 선택 데이터 연결 완료

--- frontend/src/components/meetingroom/determinepromise/MyPromise.vue
+++ frontend/src/components/meetingroom/determinepromise/MyPromise.vue
@@ -1,116 +1,120 @@
 <template>
   <div>
-    <v-dialog
-      v-model="dialog"
-      max-width="90%"
-    >
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          block
-          v-bind="attrs"
-          v-on="on"
-          color="#536DFE"
-          class="white--text"
-        >
-          약속 일정 선택
-          <v-icon
-            right
-            dark
+    <v-subheader>내 약속 일정 선택 ( {{ count }} / {{ mrUserInfo.length }} )</v-subheader>
+    <v-container>
+      <v-dialog
+        v-model="dialog"
+        max-width="90%"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            block
+            v-bind="attrs"
+            v-on="on"
+            color="#536DFE"
+            class="white--text"
           >
-            mdi-calendar-text
-          </v-icon>
-        </v-btn>
-      </template>
-      <v-card>
-        <v-card-title>약속 일정 선택</v-card-title>
-        <v-divider></v-divider>
-        <v-container>
-          <v-row class="d-flex align-center mx-0">
+            약속 일정 선택
+            <v-icon
+              right
+              dark
+            >
+              mdi-calendar-text
+            </v-icon>
+          </v-btn>
+        </template>
+        <v-card>
+          <v-card-title>약속 일정 선택</v-card-title>
+          <v-divider></v-divider>
+          <v-container>
+            <v-row class="d-flex align-center mx-0">
+              <v-col
+                cols="12"
+                class="d-flex justify-start"
+              >
+                <span>출발 장소</span>
+              </v-col>
+              <v-col
+                cols="6"
+                class="d-flex justify-start flex-column"
+              >
+                <v-btn block @click="geofind">
+                  현재 위치
+                </v-btn>
+              </v-col>
+              <v-col
+                cols="6"
+                class="d-flex justify-start flex-column"
+              >
+                <v-btn block @click="directSelect">
+                  직접 위치 선정
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-container>
+          <v-container v-show="direct">
+            <div id="map"></div>
+          </v-container>
+          <v-container>
+            <v-row class="d-flex align-center mx-0">
+              <v-col
+                cols="12"
+                class="d-flex justify-start"
+              >
+                <span>약속 가능한 날짜</span>
+              </v-col>
+            </v-row>
+          </v-container>     
+          <v-container>
             <v-col
               cols="12"
-              class="d-flex justify-start"
+              sm="12"
             >
-              <span>출발 장소</span>
+              <v-date-picker
+                v-model="dates"
+                multiple
+                color="#536DFE"
+                full-width
+                no-title
+                :min="min"
+                :max="max"
+                :picker-date="min"
+              ></v-date-picker>
             </v-col>
-            <v-col
-              cols="6"
-              class="d-flex justify-start flex-column"
-            >
-              <v-btn block @click="geofind">
-                현재 위치
-              </v-btn>
-            </v-col>
-            <v-col
-              cols="6"
-              class="d-flex justify-start flex-column"
-            >
-              <v-btn block @click="directSelect">
-                직접 위치 선정
-              </v-btn>
-            </v-col>
-          </v-row>
-        </v-container>
-        <v-container v-show="direct">
-          <div id="map"></div>
-        </v-container>
-        <v-container>
-          <v-row class="d-flex align-center mx-0">
             <v-col
               cols="12"
-              class="d-flex justify-start"
             >
-              <span>약속 가능한 날짜</span>
+              <v-text-field
+                v-model="dates"
+                label="약속 기간"
+                prepend-icon="mdi-calendar"
+                readonly
+              ></v-text-field>
             </v-col>
-          </v-row>
-        </v-container>     
-        <v-container>
-          <v-col
-            cols="12"
-            sm="12"
-          >
-            <v-date-picker
-              v-model="dates"
-              multiple
-              color="#536DFE"
-              full-width
-              no-title
-              :min="min"
-              :max="max"
-            ></v-date-picker>
-          </v-col>
-          <v-col
-            cols="12"
-          >
-            <v-text-field
-              v-model="dates"
-              label="약속 기간"
-              prepend-icon="mdi-calendar"
-              readonly
-            ></v-text-field>
-          </v-col>
-        </v-container>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-row justify="end" class="mb-1 mx-2">
-            <v-btn
-              color="blue darken-1"
-              outlined
-              @click="confirmPormise"
-              class="mx-2"
-            >
-              확인
-            </v-btn>
-            <v-btn
-              color="red darken-1"
-              outlined
-              @click="dialog = false"
-            >
-              닫기
-            </v-btn>
-          </v-row>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+          </v-container>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-row justify="end" class="mb-1 mx-2">
+              <v-btn
+                color="blue darken-1"
+                outlined
+                @click="confirmPormise"
+                class="mx-2"
+              >
+                확인
+              </v-btn>
+              <v-btn
+                color="red darken-1"
+                outlined
+                @click="dialog = false"
+              >
+                닫기
+              </v-btn>
+            </v-row>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-container>
   </div>
 </template>
 
@@ -135,19 +139,18 @@ export default {
       max: this.roomInfo.mrDateEnd,
       direct: false,
       flag: true,
+      count: '',
     }
   },
   props: {
-    roomInfo: Array
-  },
-  watch: {
-    dates() {
-      console.log(this.dates)
-    },
+    roomInfo: Object,
+    mrUserInfo: Array,
   },
   mounted() {
     this.addKakaoMapScript()
     this.mrNo = this.$route.params.id
+    console.log(this.mrUserInfo)
+    this.countSelect()
   },
   methods: {
     directSelect(){
@@ -168,7 +171,6 @@ export default {
         this.latitude = pos.coords.latitude
         this.longitude = pos.coords.longitude
         this.textContent = this.latitude + ', ' + this.longitude
-        console.log(this.textContent)
       }, err => {
         this.textContent = err.message
       })
@@ -206,7 +208,6 @@ export default {
         this.latitude = latlng.getLat()
         this.longitude = latlng.getLng()
         addMarker(latlng)
-        console.log(latlng)
       })
       function setMarkers(map) {
         for (var i = 0; i < markers.length; i++) {
@@ -235,12 +236,24 @@ export default {
           mruUserLat: this.latitude,
           mruUserLng: this.longitude
         }
-        console.log(data)
         axios.put(`http://localhost:8000/letsmeet/meetingRoomUser/set`, data)
           .then(()=> {
             alert('일정 선택이 완료되었습니다.')
-            this.$router.push({ name: 'Main'});
-            this.$emit('refresh')
+            var li = this.mrUserInfo
+            for(var i of li){
+              if(i.uNo === this.$store.state.uNo && i.mruUserLat === null){
+                this.count ++
+                  break
+                }
+            }
+            const ref_data = {
+              mruName: this.$store.state.uName,
+              mruUNo: this.$store.state.uNo,
+              mruUserDates: this.dates.join('/'),
+              mruUserLat: this.latitude,
+              mruUserLng: this.longitude,
+            }
+            this.$emit('refresh', ref_data)
           })
           .catch(() => {
             alert('일정 선택이 실패했습니다.')
@@ -249,6 +262,15 @@ export default {
         alert("데이터를 모두 입력해주세요.")
       }
     },
+    countSelect() {
+      var count = 0;
+      var li = this.mrUserInfo
+      for(var i of li){
+          if(i.mruUserLat !== null)
+              count++;
+      }
+      this.count = count
+    }
   },
 }
 </script>
