7e3594bf5c90a4fb04662f440922d8b1bc0531f5
DESKTOP-KVCQHCD\multicampus <ghqls0702@naver.com>
2021-02-09 22:39:04 +0900

    0209_BE : store 개선

--- backend/src/main/java/com/anjanda/letsmeet/repository/dto/Store.java
+++ backend/src/main/java/com/anjanda/letsmeet/repository/dto/Store.java
@@ -1,5 +1,9 @@
 package com.anjanda.letsmeet.repository.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  * 
  * @Date : 2021. 2. 5.
@@ -12,96 +16,21 @@ package com.anjanda.letsmeet.repository.dto;
  *	- 롬복 사용해서 getter, setter 사용함 (그래서 아래 메소드들 필요없음)
  */
 
+@Getter
+@Setter
+@ToString
 public class Store {
 	private int sNo;
 	private String sName;
 	private String sCategory;
-	private String sDong;
-	private String sRoad;
-	private String sBunjiMain;
-	private String sBunjiSub;
-	private String sBuildingName;
+	private String sCategoryDetail;
 	private String sRoadAddress;
+	private String sNumber;
+	private String sPrice;
+	private String sTime;
 	private String sLat;
 	private String sLng;
 	private String sImage;
 	private double sScore;
-	public int getsNo() {
-		return sNo;
-	}
-	public void setsNo(int sNo) {
-		this.sNo = sNo;
-	}
-	public String getsName() {
-		return sName;
-	}
-	public void setsName(String sName) {
-		this.sName = sName;
-	}
-	public String getsCategory() {
-		return sCategory;
-	}
-	public void setsCategory(String sCategory) {
-		this.sCategory = sCategory;
-	}
-	public String getsDong() {
-		return sDong;
-	}
-	public void setsDong(String sDong) {
-		this.sDong = sDong;
-	}
-	public String getsRoad() {
-		return sRoad;
-	}
-	public void setsRoad(String sRoad) {
-		this.sRoad = sRoad;
-	}
-	public String getsBunjiMain() {
-		return sBunjiMain;
-	}
-	public void setsBunjiMain(String sBunjiMain) {
-		this.sBunjiMain = sBunjiMain;
-	}
-	public String getsBunjiSub() {
-		return sBunjiSub;
-	}
-	public void setsBunjiSub(String sBunjiSub) {
-		this.sBunjiSub = sBunjiSub;
-	}
-	public String getsBuildingName() {
-		return sBuildingName;
-	}
-	public void setsBuildingName(String sBuildingName) {
-		this.sBuildingName = sBuildingName;
-	}
-	public String getsRoadAddress() {
-		return sRoadAddress;
-	}
-	public void setsRoadAddress(String sRoadAddress) {
-		this.sRoadAddress = sRoadAddress;
-	}
-	public String getsLat() {
-		return sLat;
-	}
-	public void setsLat(String sLat) {
-		this.sLat = sLat;
-	}
-	public String getsLng() {
-		return sLng;
-	}
-	public void setsLng(String sLng) {
-		this.sLng = sLng;
-	}
-	public String getsImage() {
-		return sImage;
-	}
-	public void setsImage(String sImage) {
-		this.sImage = sImage;
-	}
-	public double getsScore() {
-		return sScore;
-	}
-	public void setsScore(double sScore) {
-		this.sScore = sScore;
-	}
+	
 }
