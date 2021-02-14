f88d33babaf10d1bed118aefcd170afa99b09993
goldeneastlight <goldeneastlight@gmail.com>
2021-02-15 01:21:23 +0900

    0215_FE : history mode 적용했을 때 카카오 로그인 가능한지 test

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://i4d107.p.ssafy.io';
+axios.defaults.baseURL = 'https://i4d107.p.ssafy.io';
 
 // Add a request interceptor
 axios.interceptors.request.use(function (config) {
