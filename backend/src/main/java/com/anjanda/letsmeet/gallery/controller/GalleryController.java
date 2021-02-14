1507671c0934ddab66e4d11e074436c1d0bbc9ad
김지현 <khyun7621@naver.com>
2021-02-15 00:52:24 +0900

    추억공유 사진 추가 컨트롤러 수정

--- backend/src/main/java/com/anjanda/letsmeet/gallery/controller/GalleryController.java
+++ backend/src/main/java/com/anjanda/letsmeet/gallery/controller/GalleryController.java
@@ -1,8 +1,10 @@
 package com.anjanda.letsmeet.gallery.controller;
 
+import java.io.File;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -10,7 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.anjanda.letsmeet.gallery.service.GalleryService;
 import com.anjanda.letsmeet.repository.dto.Gallery;
@@ -19,12 +23,28 @@ import com.anjanda.letsmeet.repository.dto.Gallery;
 @RequestMapping("/memory")
 public class GalleryController {
 	
+	@Value("${upload-images-path}")
+	String path;
+	
 	@Autowired
 	private GalleryService galleryService;
 	
 	/* C :: 추억 공유 사진 추가 */
 	@PostMapping("/add")
-	public ResponseEntity<String> addGallery(Gallery gallery) throws Exception {
+	public ResponseEntity<String> addGallery(@RequestParam("iFile") MultipartFile file, @RequestParam("gMrNo") int gMrNo, @RequestParam("gUNo") int gUNo) throws Exception {
+		String gImage = path + "/" + "gallery-" + gMrNo + "-"  + file.getOriginalFilename();
+//		String gImage = "C:/" + "gallery-" + gMrNo + "-" + file.getOriginalFilename();
+		
+		File dest = new File(gImage);
+		file.transferTo(dest);
+
+		gImage = "https://i4d107.p.ssafy.io/images/" + "gallery-" + gMrNo + "-"  + file.getOriginalFilename();
+
+		Gallery gallery = new Gallery();
+		gallery.setgName(gImage);
+		gallery.setgMrNo(gMrNo);
+		gallery.setgUNo(gUNo);
+		
 		System.out.println("사진 경로명 : " + gallery.getgName());
 		if(galleryService.insertGallery(gallery) > 0)
 			return new ResponseEntity<String>("추억에 사진 추가 성공", HttpStatus.OK);
@@ -37,14 +57,14 @@ public class GalleryController {
 		return new ResponseEntity<List<Gallery>>(galleryService.selectGallery(gMrNo), HttpStatus.OK);
 	}
 	
-	/* U :: 추억 공유 사진 수정 */
-	@PutMapping("/edit")
-	public ResponseEntity<String> updateGallery(Gallery gallery) throws Exception {
-		System.out.println("수정될 사진 번호 " + gallery.getgNo());
-		if(galleryService.updateGallery(gallery) > 0)
-			return new ResponseEntity<String>("사진 수정 성공", HttpStatus.OK);
-		return new ResponseEntity<String>("사진 수정 실패", HttpStatus.NO_CONTENT);
-	}
+//	/* U :: 추억 공유 사진 수정 */
+//	@PutMapping("/edit")
+//	public ResponseEntity<String> updateGallery(Gallery gallery) throws Exception {
+//		System.out.println("수정될 사진 번호 " + gallery.getgNo());
+//		if(galleryService.updateGallery(gallery) > 0)
+//			return new ResponseEntity<String>("사진 수정 성공", HttpStatus.OK);
+//		return new ResponseEntity<String>("사진 수정 실패", HttpStatus.NO_CONTENT);
+//	}
 	
 	/* D :: 추억 공유 사진 삭제 */
 	@DeleteMapping("/delete")
